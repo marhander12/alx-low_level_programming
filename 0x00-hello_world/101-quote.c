4feefe2822b8bb94405369fce615e49cad97c047
Dereje Buzualem <marhander12@gmail.com>
2022-10-13 16:10:28 -0700

    task-8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,13 @@
-#include <stdio.h>
+#include <stdio,h>
 #include <unistd.h>
 /**
- * main - Entry point
- * Return: Always 0
+ * main -Entry
+ * Return: 1
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
+	wrire(1, "and that piece of art is useful\"
+- Dora Korpar,2005-10-19\n", 59);
 	return (1);
-}
+
+
