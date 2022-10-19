08902dd5c3fb4e886ac18fdce48f23fdb3a78f38
dereje buzualem <marhander12@gmail.com>
2022-10-19 06:51:13 +0000

    task-12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
-  *main - check the code
+  * print_times_table - Prints a multiplication table up to param
+  * @n: The number to be treated
   *
-  * Return: always 
+  * Return: Number matrix
   */
 void print_times_table(int n)
 {
