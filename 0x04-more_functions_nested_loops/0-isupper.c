e69ebe5a8ac9a53d6bcdfbd99803ecab01d5efbb
dereje buzualem <marhander12@gmail.com>
2022-10-21 11:49:42 +0000

    task-0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -6,13 +6,12 @@
   *
   * Return: 1 for upper letter or 0 for any else
   */
-int _isupper(int x)
+int _isupper(int c)
 {
-	if (x >= 65 && x <= 90)
-	{
-		return (1);
-	}
+	if (c >= 'A' && C <= 'Z')
+	      return (1);
+	else
 
-	return (0);
+	     return (0);
 }
 
