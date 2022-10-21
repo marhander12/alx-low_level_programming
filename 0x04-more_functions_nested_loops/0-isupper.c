d7baef1f71da2fa0569c8e9c482ae68d23a76891
dereje buzualem <marhander12@gmail.com>
2022-10-21 12:34:43 +0000

    task-0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,7 +2,7 @@
 
 /**
   * _isupper - Check if a letter is upper
-  * @x: The number to be checked
+  * @c: The number to be checked
   *
   * Return: 1 for upper letter or 0 for any else
   */
@@ -12,7 +12,9 @@ int _isupper(int c)
 	{
 		return (1);
 	}
-
-	return (0);
+	else
+	{
+		return (0);
+	}
 }
 
