67be32f1ee3d7f10851573e20b446a8a6fd51862
Dereje Buzualem <marhander12@gmail.com>
2022-10-24 09:57:53 -0700

    task-5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,27 +8,19 @@
   */
 void rev_string(char *s)
 {
-	int i, c, k;
-	char *a, aux;
-
-	a = s;
+	char rv = s[0];
+	int c = 0;
+	int i;
 
 	while (s[c] != '\0')
-	{
 		c++;
+	for (i = 0; i < c; i++)
+	{
+		c--;
+		rv = s[i];
+		s[i] = s[c];
+		s[c] = rv;
 	}
 
-	for (k = 1; k < c; k++)
-	{
-		a++;
-	}
-
-	for (i = 0; i < (c / 2); i++)
-	{
-		aux = s[i];
-		s[i] = *a;
-		*a = aux;
-		a--;
-	}
 }
 
